77cb9619a48b7957246b95e45b42466bb13cf461
Stanislav Gurniak <vainoord@gmail.com>
2022-12-19 20:47:20 +0100

    added CI hw_9.3

---
+++ 09-ci-03-CICD/example/fail.py
@@ -0,0 +1,13 @@
+def increment(num):
+    num += 1
+    return num
+def get_square(numb):
+    return numb*numb
+def print_numb(numb):
+    print("Number is {}".format(numb))
+    pass
+
+index = 0
+while (index < 10):
+    index = increment(index)
+    print(get_square(index))
